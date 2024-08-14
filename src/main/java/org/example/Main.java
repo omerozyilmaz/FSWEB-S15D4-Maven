7aa6a4b6ae0e19f869836bcc0f948b6fcd07fc9e
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2024-08-15 00:22:10 +0300

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ package org.example;
 import java.util.LinkedList;
 import java.util.Locale;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,12 +13,31 @@ public class Main {
         System.out.println(sonuc);
         System.out.println(convertDecimalToBinary(21));
         System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+        System.out.println(checkForPalindrome("I did, did I?")+ "true");
+        //
+        System.out.println(checkForPalindrome("Racecar") + "true");
+        System.out.println(checkForPalindrome("hello") + "false");
+        //
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?")+ "true");
+        System.out.println(checkForPalindrome("Test") + "false");
+        System.out.println(checkForPalindrome("Welcome") + "false");
     }
     public static boolean checkForPalindrome(String string) {
         String text = string.toLowerCase(Locale.ROOT).replaceAll("[^a-z0-9]", "");
-        StringBuilder strStringBuilder = new StringBuilder(text).reverse();
-        return text.equals(strStringBuilder.toString());
-    //Stack yapisi kullanarak yapabilirdim.
+        LinkedList<Character> charList = new LinkedList<>();
+
+        for (char c : text.toCharArray()) {
+            charList.add(c);
+        }
+
+        while (charList.size() > 1) {
+            if (!charList.pollFirst().equals(charList.pollLast())) {
+                return false;
+            }
+        }
+        return true;
+
+        // Stack yapisi kullanarak yapabilirdim.
     }
     public static String convertDecimalToBinary(int num){
         LinkedList<Integer> numList = new LinkedList<>();

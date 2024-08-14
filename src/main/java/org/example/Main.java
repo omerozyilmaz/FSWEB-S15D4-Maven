e411681f960e8cb6c031c44fb4206ad1f39c5deb
Ömer ÖZYILMAZ <omerozylmaz2@gmail.com>
2024-08-14 23:57:01 +0300

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,36 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String sonuc = convertDecimalToBinary(28);
+        System.out.println(sonuc);
+        System.out.println(convertDecimalToBinary(21));
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
     }
+    public static boolean checkForPalindrome(String string) {
+        String text = string.replaceAll("[?.,!_\\- ]","").toLowerCase(Locale.ROOT);
+        StringBuilder strStringBuilder = new StringBuilder(text).reverse();
+        return text.equals(strStringBuilder.toString());
+    //Stack yapisi kullanarak yapabilirdim.
+    }
+    public static String convertDecimalToBinary(int num){
+        LinkedList<Integer> numList = new LinkedList<>();
+        while (num>0){
+            int reminder = num % 2 ;
+            numList.addFirst(reminder);
+            num = num / 2; //devam etsin diye
+        }
+        StringBuilder numStr = new StringBuilder();
+        for (int digit : numList){
+            numStr.append(digit);
+        }
+        return numStr.toString();
+    }
+
 
 }
